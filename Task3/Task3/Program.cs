efe289c6b669d1be79b789c89b73587bed2f6c33
dvlprJacob <machomen91108@gmail.com>
2016-11-23 21:11:56 +0400

    Task 3 переделал

--- Task3/Task3/Program.cs
+++ Task3/Task3/Program.cs
@@ -13,14 +13,29 @@ namespace Task3
             Console.WriteLine("The program determines the average length of a word in the text.\nInput the text.");
             string str;
             str=Console.ReadLine();
-            string[] words = str.Split(new[] { ' ', '!', '?', ':', ',', '.' }, StringSplitOptions.RemoveEmptyEntries);//todo не пойдет, используй методы класса Char (IsDigit, IsLetter итп)
+            string str2 = "";
+            List<string> Words = new List<string>();
+            int Count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsLetter(str[i]))
+                {
+                    str2 += str[i];
+                    continue;
+                }
+                if (!str2.Equals(""))
+                {
+                    Words.Add(str2);
+                    Count++;
+                    str2 = "";
+                }
+            }
             int SumOfLength = 0;
-            foreach(string s in words)
+            foreach (string s in Words)
             {
                 SumOfLength += s.Length;
             }
-            int AverageLength = SumOfLength / words.Count();
-            Console.WriteLine("Average l[]]]]]]]ength of a word in the text is {0}.\nPress any key for apl clossing . . . ",AverageLength);
+            Console.WriteLine("Average l[]]]]]]]ength of a word in the text is {0}.\nPress any key for apl clossing . . . ", SumOfLength/Count);
             Console.ReadLine();
             return;
         }
